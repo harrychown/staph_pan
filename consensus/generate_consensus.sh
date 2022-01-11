7793e30a5ccf729a089572c33f1b286dfb0117f6
harrychown <harrychown98@gmail.com>
2022-01-11 15:51:53 +0000

    Broke: invalid FASTA character warning

--- consensus/generate_consensus.sh
+++ consensus/generate_consensus.sh
@@ -2,16 +2,24 @@
 cluster_file="/data/OneDrive/bacterial_pangenome/outdata/non_ref_roary/clustered_proteins"
 fasta_file="isolate.fasta"
 
-# 1) SUBSET CLUSTER
-line=$(head -n 1 $cluster_file)
-echo $line | sed 's/[^ ]*//' | sed 's/ /\n/g' | sed '1d' > id.tmp
-seqtk subseq $fasta_file id.tmp > subseq.tmp
+rm consensus_pangenome.fa
 
-# 2) PERFORM ALIGNMENT AND GENERATE CONSENSUS
-muscle -in subseq.tmp -out aln.tmp -maxiters 1 -diags1
-# Create HMM model
-hmmbuild hmm.tmp aln.tmp
-# Create consensus
-hmmemit -c -o consensus.tmp hmm.tmp
+while read line
+do
+	# 1) SUBSET CLUSTER
+	echo $line | sed 's/[^ ]*//' | sed 's/ /\n/g' | sed '1d' > id.tmp
+	init_id=$(head -n 1 id.tmp)
+	init_name=$(echo $line | cut -d ":" -f1)
+	# Remove previous data
+	seqtk subseq $fasta_file id.tmp > subseq.tmp
 
-# 3) RENAME AND SAVE CONSENSUS SEQUENCE
+	# 2) PERFORM ALIGNMENT AND GENERATE CONSENSUS
+	muscle -in subseq.tmp -out aln.tmp -maxiters 1 -diags1
+	# Create HMM model
+	hmmbuild hmm.tmp aln.tmp
+	# Create consensus
+	hmmemit -c -o consensus.tmp hmm.tmp
+	
+	# 3) RENAME AND SAVE CONSENSUS SEQUENCE
+	sed "s/>aln-consensus/>$init_id $init_name/" consensus.tmp >> consensus_pangenome.fa
+done <$cluster_file
